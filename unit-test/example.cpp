8bfc33d66be82140aae64b446001ff4929edca68
FacundoRimmele <facurimmele@gmail.com>
2022-09-27 23:29:02 -0300

    Faltan los Testing(no sabemos como hacerlos)

--- unit-test/example.cpp
+++ unit-test/example.cpp
@@ -4,10 +4,15 @@
 
 using namespace testing;
 
-namespace foobar::tests
-{
-	TEST(ExampleTests, Example) {
-		foobar::Example example{};
-        ASSERT_THAT(example.getValue(), Eq(99));
-	}
-}
+
+
+	TEST_CLASS(UnitTest) {
+public:
+
+	TEST_METHOD(TestDividir) {
+
+	};
+	};
+	
+
+
